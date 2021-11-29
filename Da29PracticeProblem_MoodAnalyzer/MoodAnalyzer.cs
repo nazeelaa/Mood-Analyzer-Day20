83ccc0ee21a6cf7baab62cbb20cfbd3213e006dc
nazeelaa <92171051+nazeelaa@users.noreply.github.com>
2021-11-29 16:04:48 +0530

    UC1 Mood Test Analyzer pushed

---
+++ Da29PracticeProblem_MoodAnalyzer/MoodAnalyzer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Da29PracticeProblem_MoodAnalyzer
+{
+    public class MoodAnalyzer
+    {
+        public string msg;
+        //public MoodAnalyzer(string msg)
+        //{
+        //    this.msg = msg;
+        //}
+
+        public string AnalyseMood(string msg)
+        {
+            
+                if (msg.ToLower().Contains("sad"))
+                    return "HAPPY";
+
+                else
+                    return "SAD";
+            }
+            
+
+       
+        }
+    }
+
+
---
+++ Da29PracticeProblem_MoodAnalyzer/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Da29PracticeProblem_MoodAnalyzer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+        }
+    }
+}
